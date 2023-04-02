b9a7bb6e40ba0576893e0ff33dc4539ab5af5396
Julia Martinez <zeusgirl99@gmail.com>
2023-04-02 18:11:41 -0400

    adding dislikes

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,21 +1,21 @@
 export default [
     {
         "_id": "123", "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.png",
+        "handle": "@spacex", "time": "2h", "image": "https://www.spacex.com/static/images/share.jpg",
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
         "liked": false, "disliked": true, "likes": 45, "dislikes": 5396, "replies": 123, "retuits": 432
     },
     {
         "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
+        "handle": "@boringcompany", "time": "2h", "image": "https://dwglogo.com/wp-content/uploads/2019/03/1600px-The_Boring_Company_logo.png",
         "title": "The Boring Company fixing traffic",
         "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
         "liked": true, "disliked": false, "likes": 2345, "dislikes": 12, "replies": 321, "retuits": 432
     },
     {
         "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-        "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
+        "handle": "@neurolink", "time": "3h", "image": "https://pbs.twimg.com/profile_images/895407294636187648/xoppJ-eR_400x400.jpg",
         "title": "Neurolink fixes paralisis and blindness",
         "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
         "liked": true, "disliked": false, "likes": 3456,  "dislikes": 56, "replies": 345, "retuits": 654
