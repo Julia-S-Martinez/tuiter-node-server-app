f00da31a8b74ebc357728ea5dbfc8ad7d14fda79
Julia Martinez <zeusgirl99@gmail.com>
2023-04-07 22:45:48 -0400

    Remove extra connection string

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 import express from 'express';
 import HelloController from './controllers/hello-controller.js';
 import UserController from "./controllers/users/users-controller.js";
@@ -7,7 +6,8 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://127.0.0.1:27017/tuiter'
+    || 'mongodb://127.0.0.1:27017/tuiter';
+console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
